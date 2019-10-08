9aceb3f880cf94e654f9ee17896a233b355a6bc5
Steven Pritchard <steven.pritchard@onyxpoint.com>
2019-10-08 17:15:40 -0500

    Use vagrant_dir relative to Dir.pwd.

--- tasks/targets.rb
+++ tasks/targets.rb
@@ -11,7 +11,7 @@ module Bolt # rubocop:disable Style/ClassAndModuleChildren
     def initialize(opts = {})
       @status         = nil
       @ssh_config     = {}
-      @vagrant_dir    = opts['vagrant_dir'] || Dir.pwd
+      @vagrant_dir    = opts['vagrant_dir'].nil? ? Dir.pwd : File.expand_path(opts['vagrant_dir'], Dir.pwd)
       @vagrant_binary = which('vagrant')
       @winrm_regex    = Regexp.new(opts['winrm_regex'] || 'windows')
     end
